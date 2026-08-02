937cae4f129f51bc7f5ae47596f0e0829c51075b
PRINCEPRATIK2008 <princepratik66@gmail.com>
2026-08-03 01:54:28 +0530

    fIRST code in C

---
+++ 01_First.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int var; // variable declaration
+    var = 69; // variable initialization
+    printf("%d\n", var);
+    // printf("hello world");
+    return 0;
+}
