e0b8b0730d5444b01bc1f90c12e553d6d40e0a66
PRINCEPRATIK2008 <princepratik66@gmail.com>
2026-08-07 20:54:23 +0530

    completed chapter 2

--- Operators.c
+++ Operators.c
@@ -50,9 +50,59 @@ int main()
     printf("%d \n", 5 * (2 / 2) * 3);
     printf("%d \n", 5 + 2 / 2 * 3);
 
-    // RELATIONAL OPERATOR'S..
+    // Relational operators :
 
-    printf("%d \n", 4 == 4);
+    printf("%d \n", 4 == 4); // true == 1
+
+    printf("%d \n", 4 > 4); // false == 0
+
+    printf("%d \n", 4 != 3); // != not equal to..
+
+    // Logical operators :
+
+    /*
+    && --> AND (all conditions should be fullfiled)
+    || --> OR (ek condition se bhi kaam chaljayga )
+    ! --> NOT
+    */
+
+    printf("%d \n", 5 > 2 && 4 > 3); // Result -- true as both conditions are true..
+    printf("%d \n", 3 < 2 || 5 > 1);
+
+    // Assignment operators :
+    /*
+      opr -->  = , +=, -=, *=, /=, %=
+    */
+
+    int k = 3;
+    int l = 2;
+    printf("%d \n", k %= l); // same with -, /, *, +
+
+    // write program to know if a number is divisible by 2 or not;
+
+    int x;
+    printf("enter a number: ");
+    scanf("%d", &x);
+    printf("%d ", x % 2 == 0);
+
+    /* write program to know if a number is even or odd;
+         even --> 1 , odd --> 0
+ */
+    int y;
+    printf("enter a number: ");
+    scanf("%d", &y);
+    printf("%d", y % 2 == 0);
+
+    // question 
+    int isMonday = 0;
+    int isRaining = 1;
+    printf("%d \n", isMonday || isRaining);
+
+    // question -> if a no is greater than 9 and less than 100 so the condn is true
+    int u;
+    printf("enter a number: ");
+    scanf("%d", &u);
+    printf("%d \n", u>9 && u <100);
+
     return 0;
 }
- 
