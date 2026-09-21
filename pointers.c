e793019220a65ebc72d2ae66128e80c12f3fa01a
PRINCEPRATIK2008 <princepratik66@gmail.com>
2026-09-21 11:22:00 +0530

    feat(c): more on pointers and swap function

--- pointers.c
+++ pointers.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
 
+int sum(int *, int *);
+void swap(int *, int *);
+
+int sum(int *a, int *b)
+{
+    return *a + *b;
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main()
 {
-    int x = 10;
-    int *ptr = &x; //*ptr ==> pointer var jiska kaam kisi hor var ka address store kre...
+    int i = 72;
+    int *j = &i;
+    printf("Address of i is %p\n", &i);
+    printf("Address of i is %p\n", j);
+    printf("Value at address j is: %d\n", *(&i));
 
-    printf("Address of x : %p\n", ptr); // %p se hexadecimal me address ayga
-    printf("Address of x : %u\n", ptr); // %u se sirf no milnege address me 
+    int m = 45;
+    int *k = &m;
+    int **n = &k;
 
-    printf("Value of x : %u\n", x);
-    printf("Value of x : %u\n", *ptr);
-    
+    printf("Value of i is : %d\n", i);
+    printf("Value of i is : %d\n", *j);
+
+    int x = 1, y = 7;
+    printf("Sum is : %d\n", sum(&x, &y));
+
+    printf("Before swap: x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("After swap: x = %d, y = %d\n", x, y);
+
     return 0;
 }
